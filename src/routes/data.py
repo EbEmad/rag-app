35810579d33bc67300c8773c9bef71402b2062d1
Ebrahim Emad <ebemad8@gmail.com>
2025-09-26 14:52:11 +0300

    complete structure

--- src/routes/data.py
+++ src/routes/data.py
@@ -5,6 +5,8 @@ from helpers.config import get_settings,Settings
 from controllers import DataController,ProjectController
 import aiofiles
 from models import ResponseSignal
+import logging
+logger = logging.getLogger('uvicorn.error')
 data_router=APIRouter(
     prefix='/api/v1/data',
     tags=['/api_v1','data']
@@ -15,7 +17,8 @@ data_router=APIRouter(
 async def upload_data(project_id:str,file:UploadFile,
                       app_settings:Settings=Depends(get_settings)):
     # validate the file properties
-    is_valid,result=DataController().validate_uploaded_file(file=file)
+    data_controler=DataController()
+    is_valid,result=data_controler.validate_uploaded_file(file=file)
     if not is_valid:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -26,13 +29,24 @@ async def upload_data(project_id:str,file:UploadFile,
         )
 
     project_dir_path=ProjectController().get_project_path(project_id=project_id)
-    file_path=os.path.join(
-        project_dir_path,
-        file.filename
+    file_path=data_controler.generate_unique_filename(
+        orig_file_name=file.filename,
+        project_id=project_id
     )
-    async with aiofiles.open(file_path,"wb") as f:
-        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)
+    try:
+        async with aiofiles.open(file_path,"wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+        
+        logger.error(f"Error while uploading file: {e}")
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            conent={
+                "signal":ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+
+        )
     return JSONResponse(
         content={
             'signal':ResponseSignal.FILE_UPLOAD_SUCCESS.value

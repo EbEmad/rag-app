7a7a67970c1ea216aabc3c956450966ae1e09217
Ebrahim Emad <ebemad8@gmail.com>
2025-09-26 12:42:31 +0300

    update structure

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,8 @@
 from .BaseController import BaseController
+from .ProjectController import ProjectController
 from fastapi import UploadFile
-
+from models import ResponseSignal
+import re
 class DataController(BaseController):
     def __init__(self):
         super().__init__()
@@ -8,8 +10,21 @@ class DataController(BaseController):
     
     def validate_uploaded_file(self,file:UploadFile):
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False
+            return False,ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
 
         if file.size > self.app_settings.FILE_MAX_SIZE*self.size_scale:
-            return False
-        return True
+            return False,ResponseSignal.FILE_SIZE_EXCEEDED.value
+        return True,ResponseSignal.FILE_VALIDATED_SUCCESS
+    
+    def generate_unique_filename(self,orig_file_name:str,project_id:str):
+        random_filename=self.generate_random_string()
+        project_path=ProjectController().get_project_path(project_id=project_id)
+    def get_clean_file_name(self, orig_file_name: str):
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name

53c127d62d35ffeba2acfe9755eacbb28979dfb8
Ebrahim Emad <ebemad8@gmail.com>
2025-10-05 14:16:00 +0300

    adding asset

--- src/models/db_schemes/project.py
+++ src/models/db_schemes/project.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel,Field,validator
 from typing import Optional
 from bson.objectid import ObjectId
 class Project(BaseModel):
-    _id:Optional[ObjectId]=Field(None,alias='_id')
+    id:Optional[ObjectId]=Field(None,alias='_id')
     project_id:str=Field(...,min_length=1)
 
     @validator("Project_id")
